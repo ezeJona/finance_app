3ae777a0e0ce19797d7dc43de3dafa947c0878e4
jonathan <26martinez.508@gmail.com>
2026-05-27 16:59:06 -0600

    supabase configuration

--- lib/backend-api/init_supabase.dart
+++ lib/backend-api/init_supabase.dart
@@ -1,6 +1,7 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
+import '../supabase_configuration.dart';
 
 Future<void> initSupabase() async {
   // run this in main after WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(url: 'https://ettjyqzimhpvljdmtpiq.supabase.co', anonKey: 'sb_publishable_ajZ1iDfwIYqU8Sq1QLCvgA_aUAmtufh');
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
 }
